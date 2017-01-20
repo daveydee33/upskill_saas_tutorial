20ff47a4ffe67c5f6b34b61d8af2fd814a508179
Dave Degeatano <davey.dee33@gmail.com>
2017-01-20 06:59:09 +0000

    Added form validations to model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,6 @@
 class Contact < ActiveRecord::Base
-    
+  # applying rules to a new Contact object - require these values
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :comments, presence: true
 end
